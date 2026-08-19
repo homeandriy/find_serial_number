029d4491caacdfcf462effb283f06989208d9a23
Andriy Beznosko <homeandriy@gmail.com>
2026-08-19 22:44:16 +0300

    fix-configure-portable-tesseract-data

--- config/serial-number.php
+++ config/serial-number.php
@@ -2,6 +2,7 @@
 return [
     'image_directory' => env('SERIAL_IMAGES_PATH', base_path('images')),
     'tesseract_binary' => env('TESSERACT_BINARY', 'tesseract'),
+    'tessdata_directory' => env('TESSDATA_DIRECTORY', base_path('tessdata')),
     'tesseract_languages' => env('TESSERACT_LANGUAGES', 'eng+ukr'),
     'tesseract_page_segmentation_mode' => (int) env('TESSERACT_PSM', 6),
 ];
--- setup-windows.bat
+++ setup-windows.bat
@@ -3,10 +3,16 @@ setlocal
 set "PROJECT_DIR=%~dp0"
 set "PHP_EXE=F:\Tools\php-8_5\php.exe"
 set "COMPOSER_PHAR=C:\Users\Andriy\.config\herd\bin\composer.phar"
+set "TESSERACT_DIR=C:\Program Files\Tesseract-OCR"
 if not exist "%PHP_EXE%" ( echo PHP 8.5 not found: %PHP_EXE% & exit /b 1 )
 if not exist "%COMPOSER_PHAR%" ( echo Composer not found: %COMPOSER_PHAR% & exit /b 1 )
+if not exist "%TESSERACT_DIR%\tesseract.exe" ( echo Install Tesseract OCR first: winget install --id UB-Mannheim.TesseractOCR --exact & exit /b 1 )
 cd /d "%PROJECT_DIR%"
 "%PHP_EXE%" "%COMPOSER_PHAR%" install --no-interaction --prefer-dist
+if errorlevel 1 exit /b 1
+if not exist "tessdata" mkdir tessdata
+copy /Y "%TESSERACT_DIR%\tessdata\eng.traineddata" "tessdata\eng.traineddata" >nul
+if not exist "tessdata\ukr.traineddata" curl.exe -fL -o "tessdata\ukr.traineddata" https://github.com/tesseract-ocr/tessdata_fast/raw/main/ukr.traineddata
 if errorlevel 1 exit /b 1
 "%PHP_EXE%" artisan native:install --force --no-interaction
 if errorlevel 1 exit /b 1
